f2e07dd62cf31f8b8d44cccbe251486ca890c8fe
nandhakumar20 <47220532+nandhakumar20@users.noreply.github.com>
2019-02-04 09:50:05 +0530

    Update even or odd.py

--- even or odd.py
+++ even or odd.py
@@ -1,5 +1,7 @@
 n = int(input())
-if (n % 2) == 0:
-   print("even".format(n))
+if (n < 0):
+   print("invalid")
+elif (n % 2) ==0:
+   print("Even")
 else:
-   print("Odd".format(n))
+   print("Odd")
